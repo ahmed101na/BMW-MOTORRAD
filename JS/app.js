7b8fc74d61a99b5cdcac797d34c1acca3ffebaea
ahmed101an <ahmed55nasser010@gmail.com>
2024-10-02 10:37:07 +0300

    1+

---
+++ JS/app.js
@@ -0,0 +1,17 @@
+// Recall items.
+let bike = document.getElementById("bike");
+let red_btn = document.getElementById("red");
+let blue_btn = document.getElementById("blue");
+let black_btn = document.getElementById("black");
+
+// Set the event on each element separately.
+red_btn.onclick = function () {
+    bike.style.backgroundImage = "url(../IMAGES/BMW1.png)"
+};
+blue_btn.onclick = function () {
+    bike.style.backgroundImage = "url(../IMAGES/BMW2.png)"
+};
+black_btn.onclick = function () {
+    bike.style.backgroundImage = "url(../IMAGES/BMW3.png)"
+};
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # BMW-MOTORRAD
+
+### - Computer Site Template :-
+
+![Mobile Site Template](/IMAGES/c.gif)
+
+# - Comments !!
+- This site is not responsive !.. I wrote it just to practice on the latest.
+- Live Silver website link => ( )
